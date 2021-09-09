6b9ea2d8001b800b88cf2afb4d80cdc7f9b3527e
Dorofei1543 <dorofei1543@gmail.com>
2021-09-10 00:31:41 +0300

    feat: get-email

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+export default function getEmailDomain(email) {
+    let ind = email.split('').lastIndexOf("@");
+    return email.slice(ind+1)
   // remove line with error and write your code here
 }
