4594249551fd35082ebe35f4dda98254730b71cc
Dorofei1543 <dorofei1543@gmail.com>
2021-09-10 14:49:27 +0300

    feat: sum-digits

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,32 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+export default function sortByHeight(arr ) {
+  if(!arr.includes(-1)) {
+    return arr.sort((a,b)=>{
+      if(a>b ){
+        return 1
+      }
+      return -1
+    })
+  }
+  const arrRes = arr.filter(i => i !== -1).sort((a,b)=>{
+    if(a>b ){
+      return 1
+    }
+    return -1
+  });
+  let j = 0;
+  outer: for (let i = 0; i < arrRes.length; i++){
+    for (;j < arr.length; j++){
+      if(arr[j] !== -1) {
+        arr[j] = arrRes[i];
+        console.log(j)
+        j++
+        continue outer;
+      }
+    }
+  }
+  return arr
   // remove line with error and write your code here
 }
