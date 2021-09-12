b17ece3804e511ed57650606ca0a7ef9238116d7
Dorofei1543 <dorofei1543@gmail.com>
2021-09-12 13:47:19 +0300

    feat: vigenere-chiper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,65 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(res = true){
+    this.res = res;
+  }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let resStr = '';
+    key = key.toLowerCase();
+    message = message.toLowerCase();
+    while(key.length < message.length){
+      for (let i = 0; key.length < message.length; i++){
+        key += key[i];
+        if( i == key.length - 1 ){
+          i == 0;
+        }
+      }
+    }
+    for (let i = 0, j= 0; resStr.length !== message.length; i++, j++){
+      if(message.charCodeAt(i) < 97 || message.charCodeAt(i) > 122) {
+        resStr+=message[i];
+        j--;
+        continue
+      }
+      resStr += String.fromCharCode((message.charCodeAt(i)-97 + (key.charCodeAt(j)-97))%26 +97).toUpperCase()
+    }
+    if(this.res) {
+      return resStr;
+    }
+    return resStr.split('').reverse().join('');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let resStr = '';
+    key = key.toLowerCase();
+    message = message.toLowerCase();
+    while(key.length < message.length){
+      for (let i = 0; key.length < message.length; i++){
+        key += key[i];
+        if( i == key.length - 1 ){
+          i == 0;
+        }
+      }
+    }
+    for (let i = 0, j= 0; resStr.length !== message.length; i++, j++){
+      if(message.charCodeAt(i) < 97 || message.charCodeAt(i) > 122) {
+        resStr+=message[i];
+        j--;
+        continue
+      }
+      resStr += String.fromCharCode((message.charCodeAt(i)-97 - (key.charCodeAt(j)-97) + 26)%26 +97).toUpperCase()
+    }
+    if(this.res) {
+      return resStr;
+    }
+    return resStr.split('').reverse().join('');
     // remove line with error and write your code here
   }
 }
