5cab62278a582fbe835e54b0b55c33823bde2126
Dorofei154 <Dorofei154@gmail.com>
2021-09-12 16:25:18 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  if(options.hasOwnProperty('addition')){
+    if(options.additionRepeatTimes && options.additionRepeatTimes > 1){
+      let arrAddition = [];
+      while(arrAddition.length < options.additionRepeatTimes){
+        arrAddition.push(String(options.addition))
+      }
+      if(options.additionSeparator){
+       arrAddition = arrAddition.join(options.additionSeparator);
+      }else{
+        arrAddition = arrAddition.join('|')
+      }
+      str += arrAddition;
+    }
+    else{
+      str+=options.addition
+    }
+  }
+  if(options.repeatTimes && options.repeatTimes > 1){
+    let arrStr = [];
+    while (arrStr.length < options.repeatTimes){
+      arrStr.push(str);
+    }
+    if(options.separator){
+      arrStr = arrStr.join(options.separator);
+    }else{
+      arrStr = arrStr.join('+')
+    }
+    str = arrStr
+  }
+  return str
   // remove line with error and write your code here
 }
